f50daea4fd334f4d85537d40f81f1c7cc7506945
garciajordy <jordygarcia0119@gmail.com>
2021-07-26 20:08:00 +0300

    Add tests for header when logged in and when not logged in

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
-  describe "GET /index" do
+  include Devise::Test::IntegrationHelpers
+  describe "GET /index when not logged in" do
     it "Should get index page" do
       get root_path
       expect(response.status).to be(200)
@@ -10,6 +11,42 @@ RSpec.describe "StaticPages", type: :request do
     it "Should display header" do
       get root_path
       expect(response.body).to include("Welcome to the tax calculator app")
+    end
+
+    it "Should display login and signup links when not logged in" do
+      get root_path
+      expect(response.body).to include("Sign in" && "Sign up")
+    end
+
+    it "Shouldn't display logout and My account links when signed in" do
+      get root_path
+      expect(response.body).to_not include("Sign out" && "My Account")
+    end
+  end
+
+  describe "GET /index when logged in" do
+    before do
+      user = User.create(email: "test@example.com", password: "123456")
+      sign_in(user)
+    end
+    it "Should get index page" do
+      get root_path
+      expect(response.status).to be(200)
+    end
+    
+    it "Should display header" do
+      get root_path
+      expect(response.body).to include("Welcome to the tax calculator app")
+    end
+
+    it "Shouldn't display login and signup links when not logged in" do
+      get root_path
+      expect(response.body).to_not include("Sign in" && "Sign up")
+    end
+
+    it "Should display logout and My account links when signed in" do
+      get root_path
+      expect(response.body).to include("Sign out" && "My Account")
     end
   end
 end

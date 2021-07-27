216f9cfdf96f5f28bdfec5e5cd809972ba7df2f8
garciajordy <jordygarcia0119@gmail.com>
2021-07-27 14:58:16 +0300

    Modify output and total from the Basket class to include sales_tax

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -6,10 +6,10 @@ class Basket < ApplicationRecord
   has_many :line_items, dependent: :destroy
 
   def output
-    line_items.map(&:output)
+    line_items.map(&:output) << "Sales tax: #{total_sales_tax}" << "Total: #{total}"
   end
 
   def total
-    line_items.sum(&:price).round(2)
+    (line_items.sum(&:price) + total_sales_tax.to_f).round(2)
   end
 end
--- spec/models/basket_spec.rb
+++ spec/models/basket_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Basket, type: :model do
       LineItem.create(quantity: 1, name: 'bottle of perfume', price: 18.99, basket_id: basket.id)
       LineItem.create(quantity: 1, name: 'packet of headache pills', price: 9.75, basket_id: basket.id)
       LineItem.create(quantity: 1, name: 'box of imported chocolates', price: 11.25, basket_id: basket.id)
-      output = ['1 imported bottle of perfume: 32.19', '1 bottle of perfume: 20.89', '1 packet of headache pills: 9.75', '1 box of imported chocolates: 11.85']
+      output = ['1 imported bottle of perfume: 32.19', '1 bottle of perfume: 20.89', '1 packet of headache pills: 9.75', '1 box of imported chocolates: 11.85', 'Sales tax: 6.70', 'Total: 74.68']
 
       expect(basket.output).to match(output)
     end
@@ -24,11 +24,10 @@ RSpec.describe Basket, type: :model do
     it 'returns the total of all line_items' do
       user = User.create(email: 'test@example.com', password: '123456')
       basket = described_class.create(name: 'test_basket', user_id: user.id)
-      LineItem.create(quantity: 1, name: 'imported box of chocolate', price: 14.99, basket_id: basket.id)
-      LineItem.create(quantity: 1, name: 'imported perfume', price: 5.49, basket_id: basket.id)
-      LineItem.create(quantity: 1, name: 'imported cheese', price: 10, basket_id: basket.id)
+      LineItem.create(quantity: 1, name: 'imported box of chocolate', price: 10.00, basket_id: basket.id)
+      LineItem.create(quantity: 1, name: 'imported bottle of perfume', price: 47.50, basket_id: basket.id)
 
-      expect(basket.total).to eq(30.48)
+      expect(basket.total).to eq(65.15)
     end
   end
 end
